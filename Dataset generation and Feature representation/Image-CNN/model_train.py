d5c8148c0128b4a41907b2af6587c78bb281b03f
Lz <2420400376@qq.com>
2023-05-06 15:33:37 +0800

    Ready to delete the code folder and further optimisation with changes to some of the files.

--- Dataset generation and Feature representation/Image-CNN/model_train.py
+++ Dataset generation and Feature representation/Image-CNN/model_train.py
@@ -13,41 +13,57 @@ import deepchem as dc
 import numpy as np
 from keras.callbacks import ModelCheckpoint, EarlyStopping,ReduceLROnPlateau
 
-# Importing Data
-df = pd.read_csv('data.csv',encoding= "unicode_escape")
-df_number_smile_Tg = df.values[:]
+# Importing Polymer data file and extract the corresponding information
+'''
+csv_file : the prepared polymer data file which should contain the smiles of the polymer 
+and corresponding polymer property information.
+'''
+
+polymer_data = pd.read_csv('csv_file',encoding= "unicode_escape")
+polymer_info = polymer_data.values[:]
 
 index_list = []
 PID_list = []
 tg_list = []
 smile_list = []
-for i in range(len(df_number_smile_Tg)):
-    temp0,temp1,temp2,temp3 = df_number_smile_Tg[i]
+for i in range(len(polymer_info)):
+    temp0,temp1,temp2,temp3 = polymer_info[i]
     index_list.append(temp0)
     PID_list.append(temp1)
     tg_list.append(temp2)
     smile_list.append(temp3)
 
-# Generate datasets
-idx_tr = []
+# Generate datasets:The corresponding datasets (Train, valid, Test) were generated based on Lieconv's data segmentation.
+
+'''
+dataset_train.txt: index of training set data.
+dataset_valid.txt: index of validation set data.
+dataset_test.txt: index of test set data.
+charset: the dictionary as an ordered list of these SMILES characters.
+'''
+
+idx_train = []
 with open('dataset_train.txt') as f :
     for i in f:
-        idx_tr.append(int(i))
+        idx_train.append(int(i))
         
-idx_va = []
+idx_valid = []
 with open('dataset_valid.txt') as f :
     for i in f:
-        idx_va.append(int(i))
+        idx_valid.append(int(i))
     
-idx_te = []
+idx_test = []
 with open('dataset_test.txt') as f :
     for i in f:
-        idx_te.append(int(i))
+        idx_test.append(int(i))
 
 encodings_list = []
 targets_list = []
-featurizer = dc.feat.OneHotFeaturizer(charset = ['*', 'c', '1', '(', 'C', '2', 'N', '3', '=', 'O', ')', '4', '5', '6', '7', '8', 'F', 'n', 'S', '-', 's', '[', '@', 'H', ']', 'i', 'o', '9', '%', '0', 'P', '+', '#', 'l', 'B', 'r'],max_length = 250)
-for num in idx_tr:
+featurizer = dc.feat.OneHotFeaturizer(charset = ['*', 'c', '1', '(', 'C', '2', 'N', '3', '=', 'O', ')',
+                                                 '4', '5', '6', '7', '8', 'F', 'n', 'S', '-', 's', '[', 
+                                                 '@', 'H', ']', 'i', 'o', '9', '%', '0', 'P', '+', '#', 
+                                                 'l', 'B', 'r'],max_length = 250)
+for num in idx_train:
     temp=[]
     smiles = smile_list[num]
     temp.append(smiles) 
@@ -64,7 +80,7 @@ train_Y =  np.array(targets_list)
  
 encodings_list = []
 targets_list = []  
-for num in idx_va:
+for num in idx_valid:
     temp=[]
     smiles = smile_list[num]
     temp.append(smiles) 
@@ -80,7 +96,7 @@ Valid_Y =  np.array(targets_list)
 
 encodings_list = []
 targets_list = []     
-for num in idx_te:
+for num in idx_valid:
     temp=[]
     smiles = smile_list[num]
     temp.append(smiles) 
@@ -94,12 +110,15 @@ for num in idx_te:
 Test_X = np.array(encodings_list)
 Test_Y =  np.array(targets_list)
 
-# train model
+# Train model
+'''
+Saved_model: such as image-cnn-test.hdf5.
+'''
 img_width, img_height = 250, 37
 X_train = train_X.astype('float32') 
 X_valid = Valid_X.astype('float32') 
 reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, epsilon=1e-4, mode='min',min_lr=0.00001)
-mcp_save = ModelCheckpoint('2023_2_7_20.54_36/image-cnn-36.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+mcp_save = ModelCheckpoint('Saved_model', save_best_only=True, monitor='val_loss', mode='min')
 adam = Adam(lr=0.001)
 
 model = Sequential()
